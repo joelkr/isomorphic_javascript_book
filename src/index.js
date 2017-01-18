9809742b5f5855403bbeb91c16e83e38040821b8
Joel Kreager <joel.kreager@gmail.com>
2017-01-18 08:53:57 -0800

    Finish Chap 5

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import Hapi from 'hapi';
+// Create a server with host and port
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: function(request, reply) {
+        reply('hello world');
+    }
+});
+// Start the server
+server.start();
